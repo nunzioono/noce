fd908c94c027424ed3544a2ed5d4964a8c5dd213
Nunzio Onorati <36959525+nunzioono@users.noreply.github.com>
2023-10-25 11:33:52 +0200

    Moved logic into code_utils

--- src/state/code/code_utils.rs
+++ src/state/code/code_utils.rs
@@ -1,6 +1,11 @@
+use std::{error::Error, fs::{OpenOptions, File}, io::Write};
+
+use clipboard::{ClipboardContext, ClipboardProvider};
 use crossterm::event::{Event, KeyModifiers};
 
-use super::{CodeComponent, code_selection::CodeSelection};
+use crate::state::AppContext;
+
+use super::{CodeComponent, code_selection::CodeSelection, code::Line};
 #[derive(Default, Debug, Clone, PartialEq, Eq)]
 pub struct Point {
     x: usize,
@@ -447,5 +452,173 @@ pub fn handle_right(code_component: &mut CodeComponent, event: Event) {
         }
 
 
+    }
+}
+
+pub fn handle_cut (code_component: &mut CodeComponent) {
+    let cut;
+    if let Some(selection) = code_component.get_current().get_selection() {
+        if selection.get_start() != selection.get_end() {
+            let mut code: Vec<String> = vec![];
+            for i in selection.get_start().get_x()..selection.get_end().get_x() {
+                if let Some(line) = code_component.get_current().get_line(i) {
+                    let selected_line: String;
+                    if line.get_number() == selection.get_start().get_x() {
+                        selected_line = line.get_string()[selection.get_start().get_y()..].to_string();
+                        if let Some(current_string) = code.get(line.get_number()) {
+                            let new_string: String = current_string.replace(&selected_line, "").clone();
+                            code.push(new_string.clone());    
+                        }
+                    } else if line.get_number() == selection.get_end().get_x() {
+                        selected_line = line.get_string()[..selection.get_end().get_y()].to_string();
+                        if let Some(current_string) = code.get(line.get_number()) {
+                            let new_string: String = current_string.replace(&selected_line, "").clone();
+                            code.push(new_string.clone());    
+                        }
+                    } else {
+                        code.push(line.get_string());
+                    }
+                }
+            }
+            cut = code.join("\n").to_string();
+            let clipboard: Result<ClipboardContext, Box<dyn Error>> = ClipboardProvider::new();
+            if let Ok(mut context) =  clipboard {
+                let _ = context.set_contents(cut);
+            } 
+
+            code_component.get_mut_current().flush_selection();
+        }
+    }
+}
+
+pub fn handle_copy(code_component: &mut CodeComponent) {
+    let copy;
+    if let Some(selection) = code_component.get_current().get_selection() {
+        if selection.get_start() != selection.get_end() {
+            let mut code: Vec<String> = vec![];
+            for i in selection.get_start().get_x()..selection.get_end().get_x() {
+                if let Some(line) = code_component.get_current().get_line(i) {
+                    let selected_line: String;
+                    if line.get_number() == selection.get_start().get_x() {
+                        selected_line = line.get_string()[selection.get_start().get_y()..].to_string();
+                        if let Some(current_string) = code.get(line.get_number()) {
+                            let new_string: String = current_string.replace(&selected_line, "").clone();
+                            code.push(new_string.clone());    
+                        }
+                    } else if line.get_number() == selection.get_end().get_x() {
+                        selected_line = line.get_string()[..selection.get_end().get_y()].to_string();
+                        if let Some(current_string) = code.get(line.get_number()) {
+                            let new_string: String = current_string.replace(&selected_line, "").clone();
+                            code.push(new_string.clone());    
+                        }
+                    } else {
+                        code.push(line.get_string());
+                    }
+                }
+            }
+            copy = code.join("\n").to_string();
+            let clipboard: Result<ClipboardContext, Box<dyn Error>> = ClipboardProvider::new();
+            if let Ok(mut context) =  clipboard {
+                let _ = context.set_contents(copy);
+            } 
+        }
+    }
+}
+
+pub fn handle_paste(code_component: &mut CodeComponent) {
+    let clipboard: Result<ClipboardContext, Box<dyn Error>> = ClipboardProvider::new();
+    if let Ok(mut context) =  clipboard {
+        if let Ok(contents) = context.get_contents() {
+            contents.split("\n").for_each(|line| {
+                let number = code_component.current.get_content().into_iter().map(|line| line.get_number()).max().take().unwrap() + 1;
+                let line = Line::new(number, line.to_string());
+                let _ = code_component.current.add_line(line);
+            });                                
+        }
+    }
+}
+
+pub fn handle_save(code_component: &mut CodeComponent, context: &mut AppContext) {
+    code_component.history.use_last();
+    let code = code_component.history.get_current_code();
+    let utf8_code = code.to_string().chars().map(|char| char as u8).fold(vec![], |mut vec, char| {
+        vec.push(char);
+        vec
+    });
+    if let Some(path) = context.active_file() {
+        if path.is_file() {
+            let f = OpenOptions::new().append(true).open(path);
+            if let Ok(mut file) = f {
+                let _ = file.write_all(&utf8_code);
+            }    
+        }
+    } else if let Some(path) = context.active_file() {
+        let f = File::create(path);
+        if let Ok(mut file) = f {
+            let _ = file.write_all(&utf8_code);
+        }
+    } 
+
+}
+
+pub fn handle_undo (code_component: &mut CodeComponent) {
+    code_component.history.use_previous();
+    let code = code_component.history.get_current_code();
+    code_component.current = code.clone();
+}
+
+pub fn handle_redo(code_component: &mut CodeComponent) {
+    code_component.history.use_next();
+    let code = code_component.history.get_current_code();
+    code_component.current = code.clone();
+}
+
+pub fn handle_char(code_component: &mut CodeComponent, char: String) {
+    code_component.current.remove_cursor();
+    if let Some(current_line) = code_component.current.get_line(code_component.current.get_cursor().get_x()) {
+        code_component.current.change_line_at_cursor(current_line.get_string()[..code_component.current.get_cursor().get_y()].to_string() + &char.to_string() + &current_line.get_string()[code_component.current.get_cursor().get_y()..].to_string());    
+    }
+    let y = code_component.current.get_cursor().get_y();
+    code_component.current.get_mut_cursor().set_y(y+1);
+    code_component.current.set_cursor();
+}
+
+pub fn handle_delete(code_component: &mut CodeComponent) {
+    let last_number = code_component.current.get_content().into_iter().map(|x| x.get_number()).fold(0, |line1, line2| {
+        if line1 > line2 { line1 } else { line2 }
+    });
+    let last_line = code_component.current.get_line(last_number).unwrap();
+    code_component.current.change_line(last_line.get_number(), last_line.get_string()[..last_line.get_string().len()-1].to_string());
+}
+
+pub fn handle_enter(code_component: &mut CodeComponent) {
+    {
+        let mut_code = code_component.get_mut_current();
+        mut_code.remove_cursor();    
+    }
+    let code = code_component.get_current().clone();
+    let mut_code = code_component.get_mut_current();
+    if let Some(current_line) = code.get_content().get(code.get_cursor().get_x()) {
+        let line_number = current_line.get_number().clone();
+        let new_current_string = current_line.get_string()[..code.get_cursor().get_y()].to_string().clone();
+        let new_generated_string = current_line.get_string()[code.get_cursor().get_y()..].to_string().clone();
+        mut_code.flush();
+        for number in 0 .. line_number {
+            if let Some(line) = code.get_line(number) {
+                mut_code.add_line(line.clone());                                    
+            }
+        }
+        mut_code.add_line(Line::new(current_line.get_number(), new_current_string));
+        mut_code.get_mut_cursor().set_x(code.get_cursor().get_x());
+        mut_code.get_mut_cursor().set_y(code.get_cursor().get_y());
+        mut_code.add_line(Line::new(current_line.get_number() + 1, new_generated_string));
+        for number in current_line.get_number() + 1.. code.get_content().len() {
+            if let Some(line) = code.get_line(number) {
+                let mut new_line = line.clone();
+                new_line.set_number(number + 1);
+                mut_code.add_line(new_line.clone());                                    
+            }
+        }
+        mut_code.set_cursor();
     }
 }
--- src/state/code/mod.rs
+++ src/state/code/mod.rs
@@ -3,9 +3,8 @@ pub mod code_selection;
 pub mod code;
 pub mod code_utils;
 
-use std::{fs::{File, OpenOptions}, io::{Write, Read}, error::Error, path::PathBuf, vec};
-use self::{code::{Code, Line}, code_history::CodeHistory, code_utils::{handle_up, handle_down, handle_left, handle_right}};
-use clipboard::{ClipboardProvider, ClipboardContext};
+use std::{fs::File, io::Read, path::PathBuf};
+use self::{code::{Code, Line}, code_history::CodeHistory, code_utils::{handle_up, handle_down, handle_left, handle_right, handle_cut, handle_copy, handle_paste, handle_save, handle_undo, handle_redo, handle_char, handle_delete, handle_enter}};
 use crossterm::event::{KeyEventKind, Event, KeyCode, KeyModifiers};
 
 use super::{Component, ComponentType, AppContext};
@@ -28,164 +27,29 @@ impl Component for CodeComponent {
             if key.kind == KeyEventKind::Press {
                 match key.code {
                     KeyCode::Char(char) => {
-                        let current_code = self.get_current().get_content();
-
                         let mut char_normalized = char.clone().to_string();
                         char_normalized = char_normalized.to_lowercase().to_string();
                         if char_normalized == "x" && key.modifiers.contains(KeyModifiers::CONTROL) {
-                            let cut;
-                            if let Some(selection) = self.get_current().get_selection() {
-                                if selection.get_start() != selection.get_end() {
-                                    let mut code: Vec<String> = vec![];
-                                    for i in selection.get_start().get_x()..selection.get_end().get_x() {
-                                        if let Some(line) = current_code.get(i) {
-                                            let selected_line: String;
-                                            if line.get_number() == selection.get_start().get_x() {
-                                                selected_line = line.get_string()[selection.get_start().get_y()..].to_string();
-                                                if let Some(current_string) = code.get(line.get_number()) {
-                                                    let new_string: String = current_string.replace(&selected_line, "").clone();
-                                                    code.push(new_string.clone());    
-                                                }
-                                            } else if line.get_number() == selection.get_end().get_x() {
-                                                selected_line = line.get_string()[..selection.get_end().get_y()].to_string();
-                                                if let Some(current_string) = code.get(line.get_number()) {
-                                                    let new_string: String = current_string.replace(&selected_line, "").clone();
-                                                    code.push(new_string.clone());    
-                                                }
-                                            } else {
-                                                code.push(line.get_string());
-                                            }
-                                        }
-                                    }
-                                    cut = code.join("\n").to_string();
-                                    let clipboard: Result<ClipboardContext, Box<dyn Error>> = ClipboardProvider::new();
-                                    if let Ok(mut context) =  clipboard {
-                                        let _ = context.set_contents(cut);
-                                    } 
-
-                                    self.get_mut_current().flush_selection();
-                                }
-                            }
+                            handle_cut(self);
                         } else if char_normalized == "c" && key.modifiers.contains(KeyModifiers::CONTROL) {
-                            let copy;
-                            if let Some(selection) = self.get_current().get_selection() {
-                                if selection.get_start() != selection.get_end() {
-                                    let mut code: Vec<String> = vec![];
-                                    for i in selection.get_start().get_x()..selection.get_end().get_x() {
-                                        if let Some(line) = self.get_current().get_line(i) {
-                                            let selected_line: String;
-                                            if line.get_number() == selection.get_start().get_x() {
-                                                selected_line = line.get_string()[selection.get_start().get_y()..].to_string();
-                                                if let Some(current_string) = code.get(line.get_number()) {
-                                                    let new_string: String = current_string.replace(&selected_line, "").clone();
-                                                    code.push(new_string.clone());    
-                                                }
-                                            } else if line.get_number() == selection.get_end().get_x() {
-                                                selected_line = line.get_string()[..selection.get_end().get_y()].to_string();
-                                                if let Some(current_string) = code.get(line.get_number()) {
-                                                    let new_string: String = current_string.replace(&selected_line, "").clone();
-                                                    code.push(new_string.clone());    
-                                                }
-                                            } else {
-                                                code.push(line.get_string());
-                                            }
-                                        }
-                                    }
-                                    copy = code.join("\n").to_string();
-                                    let clipboard: Result<ClipboardContext, Box<dyn Error>> = ClipboardProvider::new();
-                                    if let Ok(mut context) =  clipboard {
-                                        let _ = context.set_contents(copy);
-                                    } 
-                                }
-                            }
-                            
+                            handle_copy(self);                            
                         } else if char_normalized == "v" && key.modifiers.contains(KeyModifiers::CONTROL) {
-                            let clipboard: Result<ClipboardContext, Box<dyn Error>> = ClipboardProvider::new();
-                            if let Ok(mut context) =  clipboard {
-                                if let Ok(contents) = context.get_contents() {
-                                    contents.split("\n").for_each(|line| {
-                                        let number = self.current.get_content().into_iter().map(|line| line.get_number()).max().take().unwrap() + 1;
-                                        let line = Line::new(number, line.to_string());
-                                        let _ = self.current.add_line(line);
-                                    });                                
-                                }
-                            }
+                            handle_paste(self);
                         } else if char_normalized == "s" && key.modifiers.contains(KeyModifiers::CONTROL) {
-                                self.history.use_last();
-                                let code = self.history.get_current_code();
-                                let utf8_code = code.to_string().chars().map(|char| char as u8).fold(vec![], |mut vec, char| {
-                                    vec.push(char);
-                                    vec
-                                });
-                                if let Some(path) = context.active_file() {
-                                    if path.is_file() {
-                                        let f = OpenOptions::new().append(true).open(path);
-                                        if let Ok(mut file) = f {
-                                            let _ = file.write_all(&utf8_code);
-                                        }    
-                                    }
-                                } else if let Some(path) = context.active_file() {
-                                    let f = File::create(path);
-                                    if let Ok(mut file) = f {
-                                        let _ = file.write_all(&utf8_code);
-                                    }
-                                } 
-    
+                            handle_save(self, context);
                         } else if char_normalized == "z" && key.modifiers.contains(KeyModifiers::CONTROL) {
-                            self.history.use_previous();
-                            let code = self.history.get_current_code();
-                            self.current = code.clone();                            
+                            handle_undo(self);                            
                         } else if char_normalized == "y" && key.modifiers.contains(KeyModifiers::CONTROL) {
-                            self.history.use_next();
-                            let code = self.history.get_current_code();
-                            self.current = code.clone();
+                            handle_redo(self);
                         } else {
-                            self.current.remove_cursor();
-                            if let Some(current_line) = self.current.get_line(self.current.get_cursor().get_x()) {
-                                self.current.change_line_at_cursor(current_line.get_string()[..self.current.get_cursor().get_y()].to_string() + &char.to_string() + &current_line.get_string()[self.current.get_cursor().get_y()..].to_string());    
-                            }
-                            let y = self.current.get_cursor().get_y();
-                            self.current.get_mut_cursor().set_y(y+1);
-                            self.current.set_cursor();
+                            handle_char(self, char_normalized);
                         }
                     },
                     KeyCode::Delete => {
-                        let last_number = self.current.get_content().into_iter().map(|x| x.get_number()).fold(0, |line1, line2| {
-                            if line1 > line2 { line1 } else { line2 }
-                        });
-                        let last_line = self.current.get_line(last_number).unwrap();
-                        self.current.change_line(last_line.get_number(), last_line.get_string()[..last_line.get_string().len()-1].to_string());
+                        handle_delete(self);
                     },
                     KeyCode::Enter => {
-                        {
-                            let mut_code = self.get_mut_current();
-                            mut_code.remove_cursor();    
-                        }
-                        let code = self.get_current().clone();
-                        let mut_code = self.get_mut_current();
-                        if let Some(current_line) = code.get_content().get(code.get_cursor().get_x()) {
-                            let line_number = current_line.get_number().clone();
-                            let new_current_string = current_line.get_string()[..code.get_cursor().get_y()].to_string().clone();
-                            let new_generated_string = current_line.get_string()[code.get_cursor().get_y()..].to_string().clone();
-                            mut_code.flush();
-                            for number in 0 .. line_number {
-                                if let Some(line) = code.get_line(number) {
-                                    mut_code.add_line(line.clone());                                    
-                                }
-                            }
-                            mut_code.add_line(Line::new(current_line.get_number(), new_current_string));
-                            mut_code.get_mut_cursor().set_x(code.get_cursor().get_x());
-                            mut_code.get_mut_cursor().set_y(code.get_cursor().get_y());
-                            mut_code.add_line(Line::new(current_line.get_number() + 1, new_generated_string));
-                            for number in current_line.get_number() + 1.. code.get_content().len() {
-                                if let Some(line) = code.get_line(number) {
-                                    let mut new_line = line.clone();
-                                    new_line.set_number(number + 1);
-                                    mut_code.add_line(new_line.clone());                                    
-                                }
-                            }
-                            mut_code.set_cursor();
-                        }
+                        handle_enter(self);
                     },
                     KeyCode::Up => {
                         self.get_mut_current().remove_cursor();
@@ -216,51 +80,15 @@ impl Component for CodeComponent {
             } if key.kind == KeyEventKind::Repeat {
                 match key.code {
                     KeyCode::Char(char) => {
-                        self.current.remove_cursor();
-                        if let Some(current_line) = self.current.get_line(self.current.get_cursor().get_x()) {
-                            self.current.change_line_at_cursor(current_line.get_string()[..self.current.get_cursor().get_y()].to_string() + &char.to_string() + &current_line.get_string()[self.current.get_cursor().get_y()..].to_string());    
-                        }
-                        let y = self.current.get_cursor().get_y();
-                        self.current.get_mut_cursor().set_y(y+1);
-                        self.current.set_cursor();
+                        let mut char_normalized = char.clone().to_string();
+                        char_normalized = char_normalized.to_lowercase().to_string();    
+                        handle_char(self, char_normalized)
                     },
                     KeyCode::Delete => {
-                        let last_number = self.current.get_content().into_iter().map(|x| x.get_number()).fold(0, |line1, line2| {
-                            if line1 > line2 { line1 } else { line2 }
-                        });
-                        let last_line = self.current.get_line(last_number).unwrap();
-                        self.current.change_line(last_line.get_number(), last_line.get_string()[..last_line.get_string().len()-1].to_string());
+                        handle_delete(self);
                     },
                     KeyCode::Enter => {
-                        {
-                            let mut_code = self.get_mut_current();
-                            mut_code.remove_cursor();    
-                        }
-                        let code = self.get_current().clone();
-                        let mut_code = self.get_mut_current();
-                        if let Some(current_line) = code.get_content().get(code.get_cursor().get_x()) {
-                            let line_number = current_line.get_number().clone();
-                            let new_current_string = current_line.get_string()[..code.get_cursor().get_y()].to_string().clone();
-                            let new_generated_string = current_line.get_string()[code.get_cursor().get_y()..].to_string().clone();
-                            mut_code.flush();
-                            for number in 0 .. line_number {
-                                if let Some(line) = code.get_line(number) {
-                                    mut_code.add_line(line.clone());                                    
-                                }
-                            }
-                            mut_code.add_line(Line::new(current_line.get_number(), new_current_string));
-                            mut_code.get_mut_cursor().set_x(code.get_cursor().get_x());
-                            mut_code.get_mut_cursor().set_y(code.get_cursor().get_y());
-                            mut_code.add_line(Line::new(current_line.get_number() + 1, new_generated_string));
-                            for number in current_line.get_number() + 1.. code.get_content().len() {
-                                if let Some(line) = code.get_line(number) {
-                                    let mut new_line = line.clone();
-                                    new_line.set_number(number + 1);
-                                    mut_code.add_line(new_line.clone());                                    
-                                }
-                            }
-                            mut_code.set_cursor();
-                        }
+                        handle_enter(self);
                     },
                     KeyCode::Up => {
                         handle_up(self, event);
